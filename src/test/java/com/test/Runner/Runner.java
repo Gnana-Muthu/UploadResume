64c3ce94dfa049995b4b92c16632662c3f0d750c
Gnana-Muthu <gnanamuthupam@gmail.com>
2025-05-13 20:37:54 +0530

    message

--- src/test/java/com/test/Runner/Runner.java
+++ src/test/java/com/test/Runner/Runner.java
@@ -1,35 +1,18 @@
 package com.test.Runner;
 
 import org.junit.runner.RunWith;
-import org.testng.annotations.AfterSuite;
-
-import com.test.Utilities.Sendmail;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources",
-		glue={"com.test.StepDefinition"},
-		plugin= {"pretty"},
-
-		dryRun=false,
-		monochrome=true,
-		strict=true
-		//tags= {"@login","@Kids","@Store","@Search"}
-		//tags= {"@Urban"}
-		)
-
-
-public class Runner extends AbstractTestNGCucumberTests {
-	@AfterSuite
-	public void test() {
-		Sendmail.sentEmail();
-	}
-
+    features = "src/test/resources",
+    glue = {"com.test.StepDefinition"},
+    plugin = {"pretty"},
+    dryRun = false,
+    monochrome = true,
+    strict = true
+)
+public class Runner {
+    // No TestNG, No AfterSuite
 }
-
-
